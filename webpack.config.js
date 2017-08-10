3d160499aa3b93002a4975806fdd16adff86442f
Alan Ho <alanho@yahoo.com>
2017-08-10 10:06:49 +0800

    Refactor

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,47 @@
 var webpack = require("webpack");
 
 module.exports = {
-    entry: "./src/index.js",
-    output: {
-        path: __dirname + "/dist/assets",
-        filename: "bundle.js",
-        publicPath: "assets"
-    }, devServer: {
-        inline: true,
-        contentBase: "./dist/",
-        port: 3000
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /(node_modules)/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: ["latest",
-                            "env",
-                            "react",
-                            "es2015"]
-                    }
-                }
-            },
-            {
-                test: /\.json$/,
-                exclude: /(node_modules)/,
-                use: ['json-loader']
-            }, {
-                test: /\.(png|jpg)$/,
-                loader: 'url-loader?limit=2000000'
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader', 'postcss-loader']
-            }, {
-                test: /\.scss$/,
-                use: ['style-loader', 'css-loader', 'sass-loader']
-            }
-        ]
+  entry: "./src/index.js",
+  output: {
+    path: __dirname + "/dist/js",
+    filename: "bundle.js",
+    publicPath: "js"
+  },
+  devServer: {
+    inline: true,
+    port: 3000,
+    proxy: {
+      '/': {
+        target: 'http://localhost:5000',
+        secure: false
+      }
     }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["latest", "env", "react", "es2015"]
+          }
+        }
+      }, {
+        test: /\.json$/,
+        exclude: /(node_modules)/,
+        use: ['json-loader']
+      }, {
+        test: /\.(png|jpg)$/,
+        loader: 'url-loader?limit=2000000'
+      }, {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader']
+      }, {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
+  }
 }
-
-
-
-
-
-
-
